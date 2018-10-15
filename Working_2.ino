ef728f178be2c5bc89b54f6b276bb12f1b253a3d
mat4539 <36279548+mat4539@users.noreply.github.com>
2018-10-15 12:54:42 -0400

    Add files via upload

---
+++ Working_2.ino
@@ -0,0 +1,59 @@
+int LED1 = 3;
+int LED2 = 4;
+int LED3 = 5;
+int LED4 = 6;
+int LED5 = 7;
+int Potentiometer = 2;
+int val = 0;
+int num_pins = 6;
+
+void setup ()  {
+      pinMode (LED1, OUTPUT); 
+      pinMode (LED2, OUTPUT);
+      pinMode (LED3, OUTPUT);
+      pinMode (LED4, OUTPUT);
+      pinMode (LED5, OUTPUT);
+      pinMode(Potentiometer, OUTPUT);
+      int i;
+
+      for (i = 0; i < num_pins; i++)  
+      pinMode(pins[i], OUTPUT);
+   } // setup
+
+void loop() {
+val = analogRead(Potentiometer);
+  
+digitalWrite(LED2,HIGH);
+delay(val);
+digitalWrite(LED1,HIGH);
+delay(val);
+
+digitalWrite(LED1,LOW);
+delay(val);
+digitalWrite(LED3,HIGH);
+delay(val);
+
+digitalWrite(LED2,LOW);
+delay(val);
+digitalWrite(LED4,HIGH);
+delay(val);
+
+digitalWrite(LED3,LOW);
+delay(val);
+digitalWrite(LED5,HIGH);
+delay(val);
+
+digitalWrite(LED5,LOW);
+delay(val);
+digitalWrite(LED3,HIGH);
+delay(val);
+
+digitalWrite(LED4,LOW);
+delay(val);
+digitalWrite(LED2,HIGH);
+delay(val);
+
+digitalWrite(LED3,LOW);
+delay(val);
+
+      } //loop
