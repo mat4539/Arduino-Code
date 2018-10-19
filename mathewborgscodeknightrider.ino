2997cb003f1ddb4fcc7175fc341c3452e5f42fac
owenpurchase <44172709+owenpurchase@users.noreply.github.com>
2018-10-19 12:29:13 -0400

    files upload

---
+++ mathewborgscodeknightrider.ino
@@ -0,0 +1,60 @@
+int LED1 = 2;
+int LED2 = 3;
+int LED3 = 4;
+int LED4 = 5;
+int LED5 = 6;
+int LED6 = 7;
+int Potentiometer = 2;
+int val = 0;
+int ledPin = 13;
+int num_pins = 6;
+
+void setup ()  {
+      pinMode (LED1, OUTPUT); 
+      pinMode (LED2, OUTPUT);
+      pinMode (LED3, OUTPUT);
+      pinMode (LED4, OUTPUT);
+      pinMode (LED5, OUTPUT);
+      pinMode(Potentiometer, OUTPUT);
+      int i;
+
+      for (i = 0; i < num_pins; i++)  
+      pinMode(ledPin, OUTPUT);
+   } // setup
+
+void loop() {
+val = analogRead(Potentiometer);
+  
+digitalWrite(LED3,HIGH);
+delay(val);
+digitalWrite(LED1,HIGH);
+delay(val);
+
+digitalWrite(LED1,LOW);
+delay(val);
+digitalWrite(LED3,HIGH);
+delay(val);
+
+digitalWrite(LED2,LOW);
+delay(val);
+digitalWrite(LED4,HIGH);
+delay(val);
+
+digitalWrite(LED3,LOW);
+delay(val);
+digitalWrite(LED5,HIGH);
+delay(val);
+
+digitalWrite(LED5,LOW);
+delay(val);
+digitalWrite(LED3,HIGH);
+delay(val);
+
+digitalWrite(LED4,LOW);
+delay(val);
+digitalWrite(LED2,HIGH);
+delay(val);
+
+digitalWrite(LED3,LOW);
+delay(val);
+}
