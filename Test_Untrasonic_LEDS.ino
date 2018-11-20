4c2df45bb9d2ff946fb2fb4119d476906a585d55
mat4539 <36279548+mat4539@users.noreply.github.com>
2018-11-20 12:07:04 -0500

    LEDS

---
+++ Test_Untrasonic_LEDS.ino
@@ -0,0 +1,32 @@
+#define trigPin 5
+#define echoPin 6
+#define led 7
+
+void setup(){ 
+Serial.begin (9600);
+pinMode(trigPin, OUTPUT);
+pinMode(echoPin, INPUT);
+pinMode(led, OUTPUT);
+}
+
+void loop(){ 
+  
+long duration, distance;
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+duration = pulseIn(echoPin, HIGH);
+distance = (duration/2) / 29.1;
+if (distance < 10)
+{ digitalWrite(led,HIGH);
+}
+else {
+digitalWrite(led,LOW);
+}
+Serial.print(distance);
+Serial.println(" cm");
+delay(500);
+
+}
